f5e289b87495b5a0bfcdcab231566863963368c7
Jordan Carlson <jwgcarlson@gmail.com>
2012-07-01 02:00:23 -0700

    Rename klt-anasazi -> klt.
    * This eliminates the old PARPACK-only klt binary, switching in the new version
      that supports multiple solvers, including Anasazi.

--- code/opsec/src/klt.cpp
+++ code/opsec/src/klt.cpp
@@ -36,31 +36,32 @@
 #  include <opsec_config.h>
 #endif
 
+#include <algorithm>
 #include <cassert>
+#include <cctype>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <unistd.h>
-
 #include <string>
 #include <vector>
 using std::string;
 using std::vector;
 
-#ifdef OPSEC_USE_MPI
-#  include <mpi.h>
-#endif
+#include <mpi.h>
 
+#include "AnasaziSolver.h"
 #include "Cell.h"
 #include "Model.h"
+#include "ParpackSolver.h"
+#include "SplitFile.h"
 #include "Survey.h"
 #include "XiFunc.h"
 #include "abn.h"
 #include "cfg.h"
-#include "eig.h"
 #include "opsec.h"
 #include "sig.h"
+#include "slp.h"
 
 const char* usage =
     "Usage: klt [SWITCHES] [OPTIONS]\n"
@@ -73,9 +74,28 @@ const char* usage =
     "  modefile=FILE  Write mode function coefficients to FILE (required)\n"
     "  evalfile=FILE  Write S/N eigenvalues to FILE (required)\n"
     "  model=NAME     Use specified model (required)\n"
-    "  coordsys=TYPE  Coordinate system; either spherical or cartesian (required)\n"
-    "  Nr,Nmu,Nphi    Spherical grid (required if coordsys = spherical)\n"
-    "  Nx,Ny,Nz       Cartesian grid (required if coordsys = cartesian)\n";
+    "  solver=NAME    Use specified eigensolver (currently parpack or anasazi)\n"
+;
+
+/* Helper class for computing local signal matrix elements. */
+class SignalMatrixFactory : public MatrixFactory {
+public:
+    SignalMatrixFactory(const XiFunc& xi, Survey* survey, int Ncells, const Cell* cells, double epsrel = 1e-5, double epsabs = 1e-10)
+        : xi(xi), survey(survey), Ncells(Ncells), cells(cells), epsrel(epsrel), epsabs(epsabs)
+    {}
+
+    void ComputeMatrixValues(const vector<int>& rows, const vector<int>& cols, real* values, int lld) {
+        ComputeSignalMatrixBlock(Ncells, rows, cols, values, lld, xi, survey, cells, epsrel, epsabs);
+    }
+
+private:
+    const XiFunc& xi;
+    Survey* survey;
+    int Ncells;
+    const Cell* cells;
+    double epsrel, epsabs;
+};
+
 
 int main(int argc, char* argv[]) {
     /* Initialize MPI, if available */
@@ -86,31 +106,31 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &me);
 #endif
 
+    /* Parse command line and configuration options */
     Config cfg = opsec_init(argc, argv, usage);
 
     /* Make sure all the necessary options are provided */
-    if(!cfg_has_keys(cfg, "coordsys,cellfile,Nmodes,modefile,evalfile", ",")) {
+    if(cfg_missing_keys(cfg, "cellfile,Nmodes,modefile,evalfile")) {
         if(me == 0) opsec_error("klt: missing configuration options\n");
-        if(me == 0) { fputs(usage, stderr); fflush(stderr); }
-        opsec_exit(1);
-    }
-
-    int coordsys = cfg_get_enum(cfg, "coordsys", "cartesian", CoordSysCartesian,
-                                                 "spherical", CoordSysSpherical,
-                                                 "", -1);
-    if(coordsys == -1) {
-        if(me == 0)
-            opsec_error("klt: missing or invalid config option: coordsys = %s\n", cfg_get(cfg, "coordsys"));
+        if(me == 0) fputs(usage, stderr);
         opsec_exit(1);
     }
 
     const char* cellfile = cfg_get(cfg, "cellfile");
-    int Nmodes = cfg_get_int(cfg, "Nmodes");
     const char* modefile = cfg_get(cfg, "modefile");
     const char* evalfile = cfg_get(cfg, "evalfile");
+    int Nmodes = cfg_get_int(cfg, "Nmodes");
 
-    double epsrel = cfg_has_key(cfg, "klt.epsrel") ? cfg_get_double(cfg, "klt.epsrel") : 1e-5;
-    double epsabs = cfg_has_key(cfg, "klt.epsabs") ? cfg_get_double(cfg, "klt.epsabs") : 1e-10;
+    /* Read cells from file */
+    int Ncells;
+    Cell* cells = ReadCells(cellfile, &Ncells);
+    if(cells == NULL)
+        opsec_exit(1);
+
+    if(Nmodes > Ncells) {
+        if(me == 0) opsec_error("klt: requesting %d modes from a %d-dimensional space\n", Nmodes, Ncells);
+        opsec_exit(1);
+    }
 
     /* Load model */
     Model* model = InitializeModel(cfg);
@@ -124,149 +144,120 @@ int main(int argc, char* argv[]) {
         opsec_exit(1);
 
     /* Open output files on root process */
-    FILE* fmode = NULL;
-    FILE* feval = NULL;
+    SplitFile fmode;
+    SplitFile feval;
     if(me == 0) {
-        fmode = fopen(modefile, "w");
-        if(fmode == NULL) {
+        fmode.open(modefile, "w");
+        if(!fmode.isopen()) {
             opsec_error("klt: could not open '%s' for writing\n", modefile);
-            opsec_exit(1);
+            opsec_abort(1);
         }
-        feval = fopen(evalfile, "w");
-        if(feval == NULL) {
+        feval.open(evalfile, "w");
+        if(!feval.isopen()) {
             opsec_error("klt: could not open '%s' for writing\n", evalfile);
-            opsec_exit(1);
+            opsec_abort(1);
         }
     }
 
-    /* Read cells from file */
-    int Ncells;
-    Cell* cells = ReadCells(cellfile, &Ncells);
-    if(cells == NULL)
-        opsec_exit(1);
+    /* Create matrix factory */
+    double epsrel = cfg_has_key(cfg, "sig.epsrel") ? cfg_get_double(cfg, "sig.epsrel") : 1e-5;
+    double epsabs = cfg_has_key(cfg, "sig.epsabs") ? cfg_get_double(cfg, "sig.epsabs") : 1e-10;
+    SignalMatrixFactory matfact(Ncells, xi, survey, cells, epsrel, epsabs);
 
-    /* Determine the local problem lengths for each process */
-    vector<int> locsizes(nprocs), locdisps(nprocs);
-    for(int i = 0; i < nprocs; i++) {
-        locsizes[i] = (Ncells/nprocs) + (i < (Ncells % nprocs));
-        locdisps[i] = (i == 0) ? 0 : locdisps[i-1] + locsizes[i-1];
-    }
-    assert(locdisps[nprocs-1] + locsizes[nprocs-1] == Ncells);
-
-    /* Total number of cells */
-    int n = Ncells;
-
-    /* Number of rows of n-by-n matrix that this process is responsible for */
-    int nloc = locsizes[me];
-
-    /* Index of first row that this process is responsible for */
-    int amin = locdisps[me];
-
-    /* Memory for local nloc-by-n block of signal matrix */
-    real* S = NULL;
-
-    if(me == 0)
-        opsec_info("Computing signal matrix elements in cell basis...\n");
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if(coordsys == CoordSysSpherical) {
-        if(!cfg_has_keys(cfg, "Nr,Nmu,Nphi", ",")) {
-            opsec_error("klt: must provide config options N{r,mu,phi}\n");
-            opsec_exit(1);
-        }
-        int Nr = cfg_get_int(cfg, "Nr");
-        int Nmu = cfg_get_int(cfg, "Nmu");
-        int Nphi = cfg_get_int(cfg, "Nphi");
-        S = ComputeSignalMatrixS(n, nloc, amin, cells, Nr, Nmu, Nphi, xi, survey, epsrel, epsabs);
-    }
-    else if(coordsys == CoordSysCartesian) {
-        if(!cfg_has_keys(cfg, "Nx,Ny,Nz", ",")) {
-            opsec_error("klt: must provide config options N{x,y,z}\n");
-            opsec_exit(1);
-        }
-        int Nx = cfg_get_int(cfg, "Nx");
-        int Ny = cfg_get_int(cfg, "Ny");
-        int Nz = cfg_get_int(cfg, "Nz");
-        S = ComputeSignalMatrixC(n, nloc, amin, cells, Nx, Ny, Nz, xi, survey, epsrel, epsabs);
-    }
-
-    /* Number of eigenvalues to compute */
-    int nev = std::min(Nmodes, n-1);
-
-    /* Number of working basis vectors to use by ARPACK */
-    int ncv = std::min(2*nev, n);
-
-    /* Local memory for eigenvalues and eigenvectors */
-    real* evals = (real*) opsec_malloc(nev*sizeof(real));
-    real* modes = (real*) opsec_malloc(nloc*ncv*sizeof(real));
-
-    if(me == 0)
-        opsec_info("Computing eigenmodes of signal matrix...\n");
-
-#ifdef OPSEC_USE_MPI
-    int nconv = peig(n, nloc, nev, ncv, 1e-5, S, evals, modes);
+    /* Select eigensolver */
+    Solver* solver = NULL;
+    string s = cfg_get(cfg, "solver");
+    if(s == "parpack") {
+#ifdef HAVE_PARPACK
+        /* TODO: parse klt.parpack.* options and pass to solver */
+        solver = new ParpackSolver(Ncells, matfact);
 #else
-    int nconv = eig(n, nev, ncv, S, evals, modes);
+        if(me == 0)
+            opsec_error("klt: no support for PARPACK solver, try recompiling OPSEC\n");
+        opsec_exit(1);
 #endif
+    }
+    else if(s == "anasazi") {
+#ifdef HAVE_ANASAZI
+        /* TODO: parse klt.anasazi.* options and pass to solver */
+        solver = new AnasaziSolver(Ncells, matfact);
+#else
+        if(me == 0)
+            opsec_error("klt: no support for Anasazi solver, try recompiling OPSEC\n");
+        opsec_exit(1);
+#endif
+    }
+    else {
+        if(me == 0)
+            opsec_error("klt: unrecognized solver: %s\n", s.c_str());
+        opsec_exit(1);
+    }
 
-    /* Free signal matrix memory as soon as possible */
-    free(S);
+    /* Solve for Nmodes eigenvalues/eigenvectors */
+    int nconv = solver->Solve(Nmodes);
 
-    if(me == 0)
-        opsec_info("Writing eigenmodes and eigenvalues to file...\n");
+    /* Check for errors */
+    if(nconv < Nmodes) {
+        if(me == 0)
+            opsec_error("klt: not all eigenvalues converged (%d, %d)\n", Nmodes, nconv);
+    }
 
     /* Prepare headers in output files */
-    real* v = NULL;     // memory for a full eigenvector on root process
     if(me == 0) {
-
-        v = (real*) opsec_malloc(n*sizeof(real));
+        opsec_info("Writing eigenmodes and eigenvalues to file...\n");
 
         Config opts = cfg_new();
         cfg_set(opts, "cellfile", cellfile);
         cfg_set_int(opts, "Ncells", Ncells);
         cfg_set_int(opts, "Nmodes", Nmodes);
+        cfg_set_int(opts, "nconv", nconv);
 
+        /* Write evalfile header */
         fprintf(feval, "# Signal matrix eigenvalues\n");
-        abn_write_header(feval, nev, REAL_FMT, opts);
+        abn_write_header(feval, nconv, REAL_FMT, opts);
 
+        /* Write modefile header */
         fprintf(fmode, "# Signal matrix eigenmodes\n");
         fprintf(fmode, "# Each mode is stored consecutively, as below:\n");
         fprintf(fmode, "#   v_1,1   v_1,2   ... v_1,n\n");
         fprintf(fmode, "#   ...\n");
-        fprintf(fmode, "#   v_nev,1 v_nev,2 ... v_nev,n\n");
-        abn_write_header(fmode, nev*n, REAL_FMT, opts);
+        fprintf(fmode, "#   v_m,1   v_m,2   ... v_m,n\n");
+        abn_write_header(fmode, nconv*Ncells, REAL_FMT, opts);
 
         cfg_destroy(opts);
     }
 
-    for(int i = 1; i <= nconv; i++) {
-        /* (The strange indexing below comes from the fact that, while PARPACK
-         * returns the 'nev' largest eigenvalues, it stores this list in
-         * _reverse_ order, i.e. smallest to largest.  We want to write the
-         * spectrum to file with the largest eigenvalue first.) */
-#ifdef OPSEC_USE_MPI
-        MPI_Gatherv(modes + (nev-i)*nloc, nloc, REAL_MPI_TYPE, v, &locsizes[0], &locdisps[0], REAL_MPI_TYPE, 0, MPI_COMM_WORLD);
-#else
-        memcpy(v, modes + (nev-i)*n, n*sizeof(real));
-#endif
+    /* Create Ncells-by-1 vector r on root process, representing a single eigenvector */
+    const slp::Context* pcontext = solver->GetContext();
+    slp::Descriptor* rdesc = pcontext->new_descriptor(Ncells, 1, Ncells, 1);
+    real* rvalues = (real*) opsec_malloc(rdesc->local_size() * sizeof(real));
+    slp::Matrix<real> r(rdesc, &rvalues[0]);
+    opsec_assert(rdesc->num_local_rows() == (me == 0 ? Ncells : 0));
+
+    /* Gather eigenvectors to root process and write to file */
+    for(int j = 0; j < nconv; j++) {
+        real lambda = solver->GetEigenvalue(j);
+        slp::Matrix<real> x = solver->GetEigenvector(j);
+
+        slp::redistribute(Ncells, 1, x, 0, 0, r, 0, 0);
+
         if(me == 0) {
-            fwrite(&evals[nev-i], sizeof(real), 1, feval);
-            fwrite(v, sizeof(real), n, fmode);
+            feval.write((char*) &lambda, sizeof(real));
+            fmode.write((char*) rvalues, Ncells*sizeof(real));
         }
     }
 
     /* Clean up nicely */
     if(me == 0) {
-        fclose(feval);
-        fclose(fmode);
-        free(v);
+        feval.close();
+        fmode.close();
     }
-    free(evals);
-    free(modes);
-    free(cells);
+    free(rvalues);
+    delete rdesc;
+    delete solver;
     delete model;
     delete survey;
+    free(cells);
     cfg_destroy(cfg);
 #ifdef OPSEC_USE_MPI
     MPI_Finalize();
